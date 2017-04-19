eb0af86af0ae04b6449b48f7ef8417ac2d42ed6a
manufg <manufg92@gmail.com>
2017-04-19 11:35:26 +0530

    animations using css anf jquery

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -40,5 +40,30 @@ $('.js--scroll-to-plans').click(function(){
     }
   });
 });
+    
+    /*animations on sroll */
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    },{
+        offset:'50%'
+    });
+    
+        $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    },{
+        offset:'50%'
+    });
+    
+        $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+    },{
+        offset:'50%'
+    });
+    
+        $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+    },{
+        offset:'50%'
+    });
 
 });
